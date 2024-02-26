85cf1dda7fd2067b8202fc043801ec964bf566c8
Tin <tindejphachon@gmail.com>
2024-02-26 10:56:42 +0700

    Add tailwind.config.js file

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      "almost-white": "#FFFDFA",
+      "dark-space-blue": "#00001A",
+      "gun-metal": "#5E607A",
+      "light-vermillion": "#F15D51",
+      silver: "#C5C6CE",
+      yellow: "#E9AA52",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
