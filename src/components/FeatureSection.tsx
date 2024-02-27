21a231fbd269f63c74ff258700a114ac7327bcdb
Tin <tindejphachon@gmail.com>
2024-02-27 13:34:54 +0700

    Add FeatureSection component

---
+++ src/components/FeatureSection.tsx
@@ -0,0 +1,20 @@
+import FeatureArticle from "./FeatureArticle";
+import { featureArticles } from "../constants/featureArticles";
+
+export default function FeatureSection() {
+  return (
+    <div className="mt-[71px] flex gap-[30px]">
+      {featureArticles.map((article, index) => {
+        return (
+          <FeatureArticle
+            number={index + 1}
+            alt={article.alt}
+            image={article.img}
+            title={article.title}
+            description={article.description}
+          />
+        );
+      })}
+    </div>
+  );
+}
