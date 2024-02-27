e2fb3318939758f68e9e8f9e97e3d1ffc35fed66
Tin <tindejphachon@gmail.com>
2024-02-27 11:36:28 +0700

    Add featureArticles constant

---
+++ src/constants/featureArticles.ts
@@ -0,0 +1,24 @@
+import RetroPC from "../assets/image-retro-pcs.jpg";
+import TopLaptop from "../assets/image-top-laptops.jpg";
+import Gaming from "../assets/image-gaming-growth.jpg";
+
+export const featureArticles = [
+  {
+    img: RetroPC,
+    alt: "retro pc",
+    title: "Reviving Retro PCs",
+    description: "What happens when old PCs are given modern upgrades?",
+  },
+  {
+    img: TopLaptop,
+    alt: "top laptop",
+    title: "Top 10 Laptops of 2022",
+    description: "Our best picks for various needs and budgets.",
+  },
+  {
+    img: Gaming,
+    alt: "gaming",
+    title: "The Growth of Gaming",
+    description: "How to pandemic has sparked fresh opportunities.",
+  },
+];
