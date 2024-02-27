6143b01f5da92c70da3e81f36aaf03f66b9f5b12
Tin <tindejphachon@gmail.com>
2024-02-27 13:35:01 +0700

    Refactor NavBar component styling

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,9 +2,9 @@ import logo from "../assets/logo.svg";
 
 export default function NavBar() {
   return (
-    <nav className="flex justify-between items-center">
-      <img src={logo} alt="logo" className="w-[64.09px] h-[39.9px]" />
-      <ul className="flex gap-[40px] text-gun-metal text-body">
+    <nav className=" flex items-center justify-between">
+      <img src={logo} alt="logo" className="h-[39.9px] w-[64.09px]" />
+      <ul className="flex gap-[40px] text-body text-gun-metal">
         <li>
           <a href="/">Home</a>
         </li>
