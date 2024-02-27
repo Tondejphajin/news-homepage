ff0fc18d989affcaf36134968e3cebe0370a4e3b
Tin <tindejphachon@gmail.com>
2024-02-27 13:35:04 +0700

    Refactor NewSection component styling

--- src/components/NewSection.tsx
+++ src/components/NewSection.tsx
@@ -4,8 +4,8 @@ import Seperator from "./Seperator";
 
 export default function NewSection() {
   return (
-    <div className="mt-[32px] ml-[24px] mr-[24px]">
-      <h1 className="font-bold text-l text-yellow">New</h1>
+    <div className="mx-[24px] mt-[32px]">
+      <h1 className="text-l font-bold text-yellow">New</h1>
       <div className="mt-[34px]">
         {newTopics.map((topic, index) => {
           return (
