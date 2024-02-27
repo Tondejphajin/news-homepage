a5d03c2692635a55419e3763a40aa9c63f01abf6
Tin <tindejphachon@gmail.com>
2024-02-27 13:33:48 +0700

    Add NavBar, HeroSection, and FeatureSection components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import FeatureSection from "./components/FeatureSection";
+import HeroSection from "./components/HeroSection";
+import NavBar from "./components/NavBar";
+
 function App() {
   return (
     <>
-      <div className="text-light-vermillion">Hello</div>
+      <div className="mx-[165px] mt-[89px] max-h-[1024px] max-w-[1440px]">
+        <NavBar></NavBar>
+        <HeroSection></HeroSection>
+        <FeatureSection></FeatureSection>
+      </div>
     </>
   );
 }
