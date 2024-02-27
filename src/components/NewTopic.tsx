01f449271311bb03c9b10c3342bb8407c83d737e
Tin <tindejphachon@gmail.com>
2024-02-27 13:35:08 +0700

    Refactor NewTopic component styles

--- src/components/NewTopic.tsx
+++ src/components/NewTopic.tsx
@@ -5,8 +5,8 @@ interface NewTopicProps {
 
 export default function NewTopic({ topic, description }: NewTopicProps) {
   return (
-    <div className="w-[302px] h-[84px] mt-[31px]">
-      <div className="font-bold text-s text-almost-white">{topic}</div>
+    <div className="mt-[31px] h-[84px] w-[302px]">
+      <div className="text-s font-bold text-almost-white">{topic}</div>
       <div className="mt-[8px] text-body text-silver">{description}</div>
     </div>
   );
