dfb808ee72be3fdc6b22c5a0ebc2e87236bc1eb3
Tin <tindejphachon@gmail.com>
2024-02-27 18:18:50 +0700

    Add XIcon component

---
+++ src/components/XIcon.tsx
@@ -0,0 +1,9 @@
+import xIcon from "../assets/icon-menu-close.svg";
+
+export default function XIcon() {
+  return (
+    <div>
+      <img src={xIcon} alt="" className="size-[30.41px]" />
+    </div>
+  );
+}
