711fe0d25504b75f7462279f1e62b633ab612be4
Tin <tindejphachon@gmail.com>
2024-02-27 13:54:56 +0700

    Update HeroSection layout

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -4,8 +4,8 @@ import NewSection from "./NewSection";
 export default function HeroSection() {
   return (
     <main className="mt-[55.1px]">
-      <div className="grid grid-cols-[700px_350px] gap-[30px]">
-        <div className="w-[700px]">
+      <div className="grid grid-cols-[730px_350px] gap-[30px]">
+        <div className="w-[730px]">
           <Banner></Banner>
         </div>
         <div className="w-[350px] bg-dark-space-blue">
