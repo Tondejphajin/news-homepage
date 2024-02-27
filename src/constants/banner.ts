e5b7680d9babc7f9fcc552c7789e5de73b2d871b
Tin <tindejphachon@gmail.com>
2024-02-27 18:18:12 +0700

    Update banner image imports to support mobile

--- src/constants/banner.ts
+++ src/constants/banner.ts
@@ -1,7 +1,9 @@
-import bannerImage from "../assets/image-web-3-desktop.jpg";
+import bannerImageWeb from "../assets/image-web-3-desktop.jpg";
+import bannerImageMobile from "../assets/image-web-3-mobile.jpg";
 
 export const banner = {
-  image: bannerImage,
+  imageWeb: bannerImageWeb,
+  imageMobile: bannerImageMobile,
   title: "The Bright Future of Web 3.0?",
   description:
     "We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. But is it really fulfilling its promise?",
