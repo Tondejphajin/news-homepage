8b761edbb4b003f4830266b6178eaa15c561f5bb
Tin <tindejphachon@gmail.com>
2024-02-27 13:59:51 +0700

    Update include a hyperlink for the title

--- src/components/FeatureArticle.tsx
+++ src/components/FeatureArticle.tsx
@@ -18,7 +18,12 @@ export default function FeatureArticle({
       <img src={image} alt={alt} className="h-[127px] w-[100px]" />
       <div className="ml-[24px] flex h-[129px] w-[226px] flex-col">
         <div className="text-m font-bold text-silver">{"0" + number}</div>
-        <div className="mt-[12px] text-xs font-extra-bold">{title}</div>
+        <a
+          href="/"
+          className="mt-[12px] text-xs font-extra-bold hover:text-light-vermillion"
+        >
+          {title}
+        </a>
         <div className="mt-[9px] text-body text-gun-metal">{description}</div>
       </div>
     </div>
