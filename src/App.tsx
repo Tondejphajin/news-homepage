5b0adfd84924510aef26941745f3002f66cd05ce
Tin <tindejphachon@gmail.com>
2024-02-27 17:41:48 +0700

    Refactor layout in App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,10 @@ import NavBar from "./components/NavBar";
 function App() {
   return (
     <>
-      <div className="flex items-center justify-center">
-        <div className="mt-[89px] max-h-[1024px] max-w-[1110px]">
-          <NavBar></NavBar>
-          <HeroSection></HeroSection>
-          <FeatureSection></FeatureSection>
-        </div>
+      <div className="mx-auto mt-[89px] flex max-h-[1024px] max-w-[1110px] flex-col items-center justify-center">
+        <NavBar></NavBar>
+        <HeroSection></HeroSection>
+        <FeatureSection></FeatureSection>
       </div>
     </>
   );
