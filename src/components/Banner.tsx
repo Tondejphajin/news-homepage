2f7b7cef2b783536a08bfb58082e6bf4fe43a806
Tin <tindejphachon@gmail.com>
2024-02-27 12:03:29 +0700

    Update Banner component styling

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -9,13 +9,13 @@ export default function Banner() {
         className="w-[730px] h-[300px]"
       />
       <div className="flex justify-between mt-[30px]">
-        <div className="font-extrabold w-[350px] h-[182px] text-6xl">
+        <div className="font-extrabold w-[350px] h-[182px] text-xl">
           {banner.title}
         </div>
-        <div className="flex flex-col w-[350px] h-[181px] ml-[30px]">
+        <div className="flex flex-col w-[350px] h-[181px] ml-[30px] text-gun-metal text-body">
           <div>{banner.description}</div>
-          <button className="bg-light-vermillion w-[185px] h-[48px] text-almost-white mt-[29px]">
-            Reader More
+          <button className="bg-light-vermillion w-[185px] h-[48px] text-almost-white mt-[29px] font-bold text-[14px] leading-[24px] tracking-[4.38px]">
+            READ MORE
           </button>
         </div>
       </div>
