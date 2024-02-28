4efaebaa2a063ac66e806ec333369cf1adff6b1f
Tin <tindejphachon@gmail.com>
2024-02-28 15:09:04 +0700

    Update min-width in App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import NavBar from "./components/NavBar";
 function App() {
   return (
     <>
-      <div className="mx-auto mt-[28px] flex max-w-[1110px] flex-col items-center justify-center md:mt-[89px]">
+      <div className="mx-auto mt-[28px] flex min-w-[375px] max-w-[1110px] flex-col items-center justify-center md:mt-[89px]">
         <NavBar></NavBar>
         <HeroSection></HeroSection>
         <FeatureSection></FeatureSection>
